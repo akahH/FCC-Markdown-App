1d1f0803001be0fd1204972acaf445809c658dbe
Miguel Ginga <miguelginga@gmail.com>
2024-03-06 16:44:39 +0000

    Full App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {marked} from "marked";
 import './App.css';
 
 function App() {
+  const [text, setText]= useState(`
+  # H1
+
+  ## H2
+
+  **bold text**
+
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  ![alt text](image.jpg)
+  `);
+
+  marked.setOptions({
+    breaks: true
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>MARKDOWN APP</h1>
+        <p>by Miguel Ginga</p>
+      </div>
+      <div className="container">
+        <div class="editor-container">
+        <p className="container-title">Editor</p>
+      <textarea id="editor" onChange={(event) => 
+        {setText(event.target.value)}}
+        value = {text}>
+        </textarea>
+        </div>
+      <div className="preview-container">
+      <p className="container-title">Preview</p>
+      <div id="preview" 
+      dangerouslySetInnerHTML={{
+        __html: marked(text),
+       }}></div>
+       </div>
+      </div>
+
     </div>
   );
 }
